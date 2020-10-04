93a2dda959741b75178aa7774c5f5476f46f48c1
zhoujq <zhjq200909>
2020-10-04 22:03:07 +0800

    添加 集资页面

--- src/test/java/com/wjsn/WjsnApplicationTests.java
+++ src/test/java/com/wjsn/WjsnApplicationTests.java
@@ -1,6 +1,6 @@
 package com.wjsn;
 
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +10,4 @@ class WjsnApplicationTests {
     void contextLoads() {
     }
 
-}
+}*/
